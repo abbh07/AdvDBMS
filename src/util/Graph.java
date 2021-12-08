3cf35bd6b75bcc89d5006b4ad973a024fddeaa10
Aakash Bhattacharya <aakashbhattacharya07@gmail.com>
2021-12-08 00:54:17 -0500

    Added docustring for Graph.java

--- src/util/Graph.java
+++ src/util/Graph.java
@@ -1,9 +1,13 @@
+/**
+ * @author Aakash Bhattacharya
+ * @version 1.0.0
+ */
 package util;
 
 import java.util.*;
 
 public class Graph {
-    private static final int totalVertices = 100;
+    private static final int totalVertices = 100000;
     private List<List<Integer>> adj;
 
     public ArrayList<Integer> getAffectedNodes() {
@@ -20,6 +24,13 @@ public class Graph {
         }
     }
 
+    /**
+     * The method performs a DFS traversal of the graph and finds a cycle.
+     * @param index Start index of the traversal
+     * @param visited Visited nodes
+     * @param recursionStack Nodes in the current stack
+     * @return Boolean identifying if a cycle is found
+     */
     private boolean dfs(int index, boolean[] visited, boolean[] recursionStack) {
         if (recursionStack[index]) {
             return true;
@@ -43,6 +54,10 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Helper to identify a cycle in the graph.
+     * @return Boolean identifying a cycle
+     */
     public boolean isCyclic() {
         boolean[] visited = new boolean[totalVertices];
         boolean[] recursionStack = new boolean[totalVertices];
@@ -56,10 +71,19 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Add edge to the graph.
+     * @param source Source node
+     * @param destination Destination node
+     */
     public void addEdge(int source, int destination) {
         adj.get(source).add(destination);
     }
 
+    /**
+     * Remove an edge from the graph.
+     * @param source Node which has to be removed
+     */
     public void removeEdge(int source) {
         adj.get(source).clear();
         for (int i = 0; i < adj.size(); i++) {
